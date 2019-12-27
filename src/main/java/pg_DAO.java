86b5753cf2ec08ccf4e24579084b0b436bbb3c1d
alimbetov <51784151+alimbetov@users.noreply.github.com>
2019-12-27 14:38:46 +0600

    Update pg_DAO.java

--- src/main/java/pg_DAO.java
+++ src/main/java/pg_DAO.java
@@ -1,7 +1,7 @@
 public interface pg_DAO {
     public static final String DB_URL = "jdbc:postgresql://192.168.188.188:5432/dodo";
     public static final String DRIVER ="org.postgresql.Driver";
-    public static final String USER = "tester";
-    public static final String PASS = "TESTER";
+    public static final String USER = "user";
+    public static final String PASS = "pw";
 
 }
